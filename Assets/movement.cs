8ec83a5372310f963c5b078193dd1b2b97115e2f
Simeon12345678 <simeon.milic@elev.ga.ntig.se>
2024-03-19 16:24:52 +0100

    astounding commit message

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    float speed = 3;
 
-    // Update is called once per frame
+    [SerializeField]
+    float jumpForce = 30;
+
+    [SerializeField]
+    float gravityMultiplier = 4;
+
+    Vector2 inputVector;
+
+    float yVelocity = 0f;
+
+    bool isJumping = false;
+
     void Update()
     {
-        
+        Vector3 movement = transform.forward * inputVector.y + transform.right * inputVector.x;
+        movement *= speed;
+
+        if (GetComponent<CharacterController>().isGrounded)
+        {
+            yVelocity = -1;
+            if (isJumping)
+            {
+                yVelocity = jumpForce;
+            }
+        }
+
+        yVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+
+        movement.y = yVelocity;
+
+        GetComponent<CharacterController>().Move(movement * Time.deltaTime);
     }
+
+    void OnMove(InputValue value) => inputVector = value.Get<Vector2>();
+    void OnJump(InputValue value) => isJumping = true;
 }
